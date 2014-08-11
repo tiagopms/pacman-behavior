715b6b76f343132d651d0af1e72140f7cd0fb3dc
Tiago <tiago@tiago-ubuntu-14.04>
2014-08-11 18:58:55 -0300

    Updating pacman_ros to use all ghosts and removing more *.pyc files

--- pacman_interface/src/pacman_ros.py
+++ pacman_interface/src/pacman_ros.py
@@ -7,7 +7,7 @@ import pacman
 def runPacman():
     rospy.init_node('pacman_interface', anonymous=True)
 
-    a=["-p", "RosAgent", "-l", "smallClassic", "-k", "1"]
+    a=["-p", "RosAgent", "-l", "smallClassic", "-k", "3"]
     args = pacman.readCommand(a)
     pacman.runGames(**args)
 
