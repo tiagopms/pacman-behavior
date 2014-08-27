6d3c6bb00c37b1f705e22f5897d6f73168ee588c
Tiago Pimentel <tiagopms@gmail.com>
2014-08-27 18:28:30 -0300

    First try on optimizing observation in particle filter

--- particle_filter_pacman/src/particle_filter_pacman/particle_filter.cpp
+++ particle_filter_pacman/src/particle_filter_pacman/particle_filter.cpp
@@ -23,6 +23,11 @@ void ParticleFilter::estimateMovement(pacman_interface::PacmanAction action)
     }
 }
 
+bool compareProbabilities(std::pair< double, GameParticle > i, std::pair< double, GameParticle > j)
+{
+    return ( i.first < j.first );
+}
+
 void ParticleFilter::observePacman(const geometry_msgs::Pose::ConstPtr& msg)
 {
     int measurement_x = msg->position.x;
@@ -31,8 +36,14 @@ void ParticleFilter::observePacman(const geometry_msgs::Pose::ConstPtr& msg)
     std::vector< double > probabilities;
     probabilities.reserve(util::NUMBER_OF_PARTICLES);
 
+    std::map< double, GameParticle > particles_map;
+    std::vector< std::pair< double, GameParticle > > particles_pair;
+    particles_pair.reserve(util::NUMBER_OF_PARTICLES);
+
     std::vector< GameParticle > new_particles;
     new_particles.reserve(util::NUMBER_OF_PARTICLES);
+
+const clock_t begin_time = clock();
 
     double sum_prob_all_particles = 0;
 
@@ -42,20 +53,41 @@ void ParticleFilter::observePacman(const geometry_msgs::Pose::ConstPtr& msg)
         double probability = util::getProbOfMeasurementGivenPosition(pose.position.x, pose.position.y, measurement_x, measurement_y, 5);
         probabilities.push_back(probability);
         sum_prob_all_particles += probability;
+
+        particles_pair.push_back(std::make_pair(sum_prob_all_particles, *it));
     }
 
     ROS_ERROR_STREAM_COND(sum_prob_all_particles == 0, "Error, all particles have a zero probability of being correct");
     ROS_ERROR_STREAM("Probs: " << sum_prob_all_particles);
     ROS_ERROR_STREAM("X: " << measurement_x << " Y: " << measurement_y);
 
+    std::sort (particles_pair.begin(), particles_pair.end(), compareProbabilities);
+
     // select new particle group
     int particlesCounter = util::NUMBER_OF_PARTICLES;
-    double random_variable = ( std::rand() / (double) RAND_MAX ) * sum_prob_all_particles;
     double sum_probs = 0;
+    double random_multiplier = sum_prob_all_particles / (double) RAND_MAX;
 
     int counter = util::NUMBER_OF_PARTICLES - 1;
 
     while (particlesCounter > 0)
+    {
+        double random_variable = std::rand() * random_multiplier;
+
+        std::vector< std::pair< double, GameParticle > >::iterator low;
+        low=std::lower_bound (particles_pair.begin(), particles_pair.end(), std::make_pair(random_variable, game_particles_[0]), compareProbabilities);
+
+        particlesCounter--;
+        new_particles.push_back(low->second);
+    }
+
+static float mean = 0;
+static int number_of_it = 1;
+mean = mean * ( ( number_of_it - 1 ) / (double) number_of_it) + (float( clock () - begin_time )  /  CLOCKS_PER_SEC) * ( ( 1 ) / (double) number_of_it);
+number_of_it++;
+std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << std::endl;
+std::cout << mean << std::endl;
+    /*while (particlesCounter > 0)
     {
        // ROS_INFO_STREAM_THROTTLE(1, "Selecting particle " << sum_probs);
       //  ROS_INFO_STREAM_THROTTLE(1, "Random " << random_variable);
@@ -64,11 +96,8 @@ void ParticleFilter::observePacman(const geometry_msgs::Pose::ConstPtr& msg)
         for(std::vector< double >::reverse_iterator it = probabilities.rbegin(); it != probabilities.rend(); ++it)
         {
             sum_probs += (double) *it;
-            if(sum_probs >= random_variable)
+            while(sum_probs >= random_variable)
             {
-
-                //ROS_ERROR_STREAM("New particle " << particlesCounter);
-
                 particlesCounter--;
                 new_particles.push_back(game_particles_[counter]);
                 random_variable += ( std::rand() / (double) RAND_MAX ) * sum_prob_all_particles;
@@ -78,7 +107,7 @@ void ParticleFilter::observePacman(const geometry_msgs::Pose::ConstPtr& msg)
             }
             counter--;
         }
-    }
+    }*/
 
     AFTER_PACMAN_PARTICLES_SELECTED:
 
