534be3e9b29e2169d18f35ce73ba6f54359e2e4d
Tiago Pimentel <tiagopms@gmail.com>
2014-09-10 22:41:14 -0300

    Q Learning: Created game package which games can be started with a service and that sends another service when game ends and another to activate games that will run the q learning algorithm

---
+++ pacman_game/src/pacman_ros.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import rospy
+import pacman
+from pacman_msgs.srv import StartGame
+from pacman_msgs.srv import EndGame
+
+
+start_game = False
+show_gui = False
+
+
+class PacmanGame():
+    """
+    A game started and controlled by ROS messages and services.
+    """
+    def __init__( self, index = 0 ):
+        #start ros node
+        rospy.init_node('pacman_game', anonymous=True)
+        self.r = rospy.Rate(10) # 10hz
+
+        # game attributes
+        a=["-p", "RosWaitAgent", "-l", "smallClassic", "-k", "4"]
+        self.args = pacman.readCommand(a)
+
+        # service and variables to start new game and end it
+        self.start_game_srv = rospy.Service('/pacman/start_game', StartGame, self.start_game_service)
+        self.end_game_client = rospy.ServiceProxy('/pacman/end_game', EndGame)
+        self.start_game = False
+        self.show_gui = False
+
+    def start_game_service(self, req):
+        if self.start_game:
+            rospy.logwarn("Trying to start already started game")
+            return False
+        self.start_game = True
+        self.show_gui = req.show_gui
+        return True
+
+    def end_game(self, is_win):
+        # set game as not running
+        self.start_game = False
+        self.show_gui = False
+
+        # call end game service
+        rospy.loginfo("Ending game!")
+        rospy.wait_for_service('/pacman/end_game')
+        try:
+          srv_resp = self.end_game_client(is_win)
+          if not srv_resp.game_restarted:
+            rospy.signal_shutdown("Shuting down node, game ended and wasn't restarted")
+        except rospy.ServiceException as exc:
+          print("Service did not process request: " + str(exc))
+
+        return True
+
+    # run game
+    def run(self):
+        is_win = False
+        while not rospy.is_shutdown():
+            if self.start_game:
+
+                #if not show gui, set game as in training mode
+                if not self.show_gui:
+                    self.args['numTraining'] = 1
+
+                # run game and get if win or lose
+                games = pacman.runGames(**self.args)
+                single_game = games.pop()
+                is_win = single_game.state.isWin()
+
+                # end game
+                self.end_game(is_win)
+
+            #sleep while in loop
+            self.r.sleep()
+
+if __name__ == '__main__':
+    game = PacmanGame()
+    try:
+        game.run()
+    except rospy.ROSInterruptException: pass
+    
--- q_learning_pacman/src/q_learning.cpp
+++ q_learning_pacman/src/q_learning.cpp
@@ -1,13 +1,108 @@
 #include "ros/ros.h"
 
+#include "pacman_msgs/PacmanAction.h"
+#include "pacman_msgs/StartGame.h"
+#include "pacman_msgs/EndGame.h"
+
+int NUMBER_OF_GAMES = 7;
+int NUMBER_OF_TRAININGS = 0;
+
+bool endGame(pacman_msgs::EndGame::Request &req, pacman_msgs::EndGame::Response &res, ros::ServiceClient *start_game_client, bool *end_program)
+{
+    // count number of games
+    static int game_count = 0;
+    game_count++;
+
+    if (req.win)
+    {
+        ROS_INFO_STREAM("Won game " << game_count);
+    }
+    else
+    {
+        ROS_WARN_STREAM("Lost game " << game_count);
+    }
+
+    if (game_count < NUMBER_OF_GAMES)
+    {
+        pacman_msgs::StartGame start_game;
+
+        if (game_count < NUMBER_OF_TRAININGS)
+            start_game.request.show_gui = false;
+        else
+            start_game.request.show_gui = true;
+
+        if (start_game_client->call(start_game))
+            if(start_game.response.started)
+            {
+                // new game started
+                ROS_INFO("Game started");
+                res.game_restarted = true;
+                return true;
+            }
+            else
+                ROS_ERROR("Failed to start game (check if game already started)");
+        else // if problem print error
+            ROS_ERROR("Failed to call service StartGame");
+
+    }
+    else
+    {
+        *end_program = true;
+    }
+
+    // game not restarted
+    res.game_restarted = false;
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "q_learning");
     ros::NodeHandle n;
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(10);
+    bool end_program = false;
 
-    while (ros::ok())
+    ros::Publisher chatter_pub = n.advertise<pacman_msgs::PacmanAction>("/pacman/pacman_action", 1000);
+
+    // client to start game service and server for end game service
+    ros::ServiceClient start_game_client = n.serviceClient<pacman_msgs::StartGame>("/pacman/start_game");
+    ros::ServiceServer end_game_service = n.advertiseService<pacman_msgs::EndGame::Request, pacman_msgs::EndGame::Response>
+                                                        ("/pacman/end_game", boost::bind(endGame, _1, _2, &start_game_client, &end_program));
+    ros::service::waitForService("/pacman/start_game", -1);
+
+    // start first game
+    pacman_msgs::StartGame start_game;
+    if (NUMBER_OF_TRAININGS)
+        start_game.request.show_gui = false;
+    else
+        start_game.request.show_gui = true;
+    if (start_game_client.call(start_game))
     {
-        
+        if(start_game.response.started)
+        {
+            ROS_INFO("Game started");
+        }
+        else
+        {
+            ROS_ERROR("Failed to start game (check if game already started)");
+        }
     }
+    else // if problem print error
+    {
+        ROS_ERROR("Failed to call service StartGame");
+    }
+
+    while (ros::ok() && !end_program)
+    {
+        // move west
+        pacman_msgs::PacmanAction action;
+        action.action = pacman_msgs::PacmanAction::WEST;
+        chatter_pub.publish(action);
+
+        // sleep and spin ros
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+
+    ros::shutdown();
 }
